68b9ac57227df85524bed0379e509f4087bafbc9
Aedulapuram Manivyas <62017824+Manivyas@users.noreply.github.com>
2023-06-10 11:53:26 +0530

    Qa to Main (#5)
    * setting-up-react
    
    * tailwind-setup
    
    * navbar-pages
    
    * navbar-pages-all
    
    * final-UI
    
    * final-changes
    
    * db addition
    
    ---------
    
    Co-authored-by: manivyas <manivyasedu@gmail.com>

---
+++ blog-ui/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import ArticlesList from "./pages/articles-list";
+import About from "./pages/about";
+import Articles from "./pages/articles";
+import Navbar from "./components/Navbar";
+import NotFound from "./pages/NotFound";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articleslist" element={<ArticlesList />} />
+          <Route path="/article/:name" element={<Articles />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ blog-ui/src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+                About
+            </h1>
+            <p className='mx-auto leading-relaxed mb-6'>
+                I am writing to express my interest in the Junior Developer - Intern position at Capgemini. As an undergraduate student pursuing a B.Tech degree in Computer Science and Engineering, I have acquired comprehensive knowledge and practical experience in various areas of Computer Science and Engineering through my coursework and projects at my university. I am eager to apply my skills and contribute to the industry.
+                I am particularly proud of my appointment as a Board Member and Project Head of the Soft Computing Research Society, a newly established club/chapter at my university, during the 2021-2022 term. This role provided me with valuable leadership experience and enhanced my ability to manage projects effectively.
+                During my internship, I had the opportunity to work on core CFS team at Yubi, as a Full Stack Developer.
+                I made significant contributions to the UI, including revamping the user interface, developing dynamic config-based filter and search components, creating tooltip and pagination functionalities, and implementing modal components.
+                Additionally, on the backend, I was responsible for versioning APIs, managing logs, integrating New Relic services, and developing custom reports and repayment service modules.
+                One of my recent achievements was adopting the Forest Admin Tool for the Custom Reports and Repayment Service within a sprint, which resulted in streamlining and enhancing the functionality of 50% of the CFS services.
+                I am confident that my technical expertise, hands-on experience, and passion for innovation make me a strong candidate for Junior Developer - Intern position at Capgemini. I am excited about the opportunity to contribute to your team and collaborate on impactful projects.
+                I look forward to the opportunity of discussing how my skills and experience align with the requirements of the role. Please find my attached resume for further details
+
+            </p>
+        </div>
+  );
+};
+
+export default About;
